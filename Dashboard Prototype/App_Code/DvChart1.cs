853ef8271f902ca685fd8f2d0b14d2b78d2376c3
Paras Parmar <parasparmar@gmail.com>
2018-04-27 02:36:23 +0530

    Add files via upload

---
+++ Dashboard Prototype/App_Code/DvChart1.cs
@@ -0,0 +1,11 @@
+﻿namespace Dashboard_Prototype
+{
+    public class DvChart1
+    {
+        public int YearOfBirth { get; set; }
+        public int Male { get; set; }
+        public int Female { get; set; }
+        public int Not_Specified { get; set; }
+
+    }
+}
