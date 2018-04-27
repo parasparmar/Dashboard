2d9a07ce43d731aa343a530b4c32e79dfd900c80
Paras Parmar <parasparmar@gmail.com>
2018-04-28 02:42:00 +0530

    Add files via upload

--- Dashboard Prototype/App_Code/DvChart1.cs
+++ Dashboard Prototype/App_Code/DvChart1.cs
@@ -7,5 +7,13 @@
         public int Female { get; set; }
         public int Not_Specified { get; set; }
 
+        //public string YearOfBirth { get; set; }
+        //public string Gender { get; set; }
+        //public string Department { get; set; }
+        //public string Role { get; set; }
+        //public string Level { get; set; }
+        //public int HeadCount { get; set; }
+
+
     }
 }
